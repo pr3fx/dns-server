fee72d2580a4b4096caad2cc012e8b0c2d78ca83
Jordan Collocott <jrdn.collocott@gmail.com>
2025-07-06 11:26:20 +0200

    feat: start UDP, use logrus logging library

--- cmd/main.go
+++ cmd/main.go
@@ -1,50 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"net"
+	log "github.com/sirupsen/logrus"
 )
 
-// Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
-var _ = net.ListenUDP
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	log.Info("Attempting to start UDP server...")
 
-	// Uncomment this block to pass the first stage
-	//
-	// udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
-	// if err != nil {
-	// 	fmt.Println("Failed to resolve UDP address:", err)
-	// 	return
-	// }
-	//
-	// udpConn, err := net.ListenUDP("udp", udpAddr)
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to address:", err)
-	// 	return
-	// }
-	// defer udpConn.Close()
-	//
-	// buf := make([]byte, 512)
-	//
-	// for {
-	// 	size, source, err := udpConn.ReadFromUDP(buf)
-	// 	if err != nil {
-	// 		fmt.Println("Error receiving data:", err)
-	// 		break
-	// 	}
-	//
-	// 	receivedData := string(buf[:size])
-	// 	fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-	//
-	// 	// Create an empty response
-	// 	response := []byte{}
-	//
-	// 	_, err = udpConn.WriteToUDP(response, source)
-	// 	if err != nil {
-	// 		fmt.Println("Failed to send response:", err)
-	// 	}
-	// }
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	if err != nil {
+		log.Error("Failed to resolve UDP address:", err)
+		return
+	}
+	log.Info("UDP address successfully resolved")
+	
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Error("Failed to bind to address:", err)
+		return
+	}
+	log.Info("Bind to address successful")
+	defer udpConn.Close()
+	
+	buf := make([]byte, 512)
+	
+	for {
+		size, source, err := udpConn.ReadFromUDP(buf)
+		if err != nil {
+			log.Error("Error receiving data:", err)
+			break
+		}
+	
+		receivedData := string(buf[:size])
+		log.Error("Received %d bytes from %s: %s", size, source, receivedData)
+	
+		// Create an empty response
+		response := []byte{}
+	
+		_, err = udpConn.WriteToUDP(response, source)
+		if err != nil {
+			log.Error("Failed to send response:", err)
+		}
+	}
 }
