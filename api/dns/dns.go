c39833ec96d347d3526427b1a724ddf07da6d31c
Jordan Collocott <jrdn.collocott@gmail.com>
2025-07-07 20:01:39 +0200

    feat(DNSMessage): add header and question attributes

--- api/dns/dns.go
+++ api/dns/dns.go
@@ -2,4 +2,14 @@ package dns
 
 
 type DNSMessage struct {
+	header DNSHeader
+	question DNSQuestion
+}
+
+func (message *DNSMessage) SetHeader(header DNSHeader) {
+	(*message).header = header
+}
+
+func (message *DNSMessage) SetQuestion(question DNSQuestion) {
+	(*message).question = question
 }
