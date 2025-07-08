bf88e2ea8f28b9c03a1ca172b8c1ccd2ce199a80
Jordan Collocott <jrdn.collocott@gmail.com>
2025-07-08 07:14:57 +0200

    feat: increment header QDCOUNT when adding DNSQuestion

--- api/dns/dns.go
+++ api/dns/dns.go
@@ -3,13 +3,15 @@ package dns
 
 type DNSMessage struct {
 	header DNSHeader
-	question DNSQuestion
+	question []DNSQuestion
 }
 
 func (message *DNSMessage) SetHeader(header DNSHeader) {
 	(*message).header = header
 }
 
-func (message *DNSMessage) SetQuestion(question DNSQuestion) {
-	(*message).question = question
+func (message *DNSMessage) AddQuestion(question DNSQuestion) {
+	(*message).question = append((*message).question, question)
+	// Increment QDCOUNT header field
+	(*message).header.SetQDCOUNT((*message).header.qdcount + 1)
 }
