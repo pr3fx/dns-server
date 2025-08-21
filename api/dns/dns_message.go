5c51e8b49d95a99380fa04c605b9688a3f2973e8
Jordan Collocott <jrdn.collocott@gmail.com>
2025-08-21 19:37:20 +0200

    feat: add DNSAnswer to DNSMessage

--- api/dns/dns_message.go
+++ api/dns/dns_message.go
@@ -9,6 +9,7 @@ import (
 type DNSMessage struct {
 	header DNSHeader
 	question []DNSQuestion
+	answer []DNSAnswer
 }
 
 func (message *DNSMessage) SetHeader(header DNSHeader) {
@@ -19,6 +20,12 @@ func (message *DNSMessage) AddQuestion(question DNSQuestion) {
 	(*message).question = append((*message).question, question)
 	// Increment QDCOUNT header field
 	(*message).header.SetQDCOUNT((*message).header.qdcount + 1)
+}
+
+func (message *DNSMessage) AddAnswer(answer DNSAnswer) {
+	(*message).answer = append((*message).answer, answer)
+	// Increment ANCOUNT header field
+	(*message).header.SetANCOUNT((*message).header.ancount + 1)
 }
 
 func (message DNSMessage) serializeQuestions() []byte {
@@ -39,14 +46,39 @@ func (message DNSMessage) serializeQuestions() []byte {
 			))
 	}
 
-	buf := make([]byte, 0, len(header_buf) + len(question_buf))
 	return question_buf
+}
+
+func (message DNSMessage) serializeAnswers() []byte {
+	answer_buf_cap := uint32(0)
+	for _, a := range message.answer {
+		answer_buf_cap += a.GetByteLen()
+	}
+	answer_buf := make([]byte, 0, answer_buf_cap)
+	for _, a := range message.answer {
+		answer_buf = append(answer_buf, a.Serialize()...)
+	}
+	if uint32(len(answer_buf)) != answer_buf_cap {
+		log.Warning(
+			fmt.Sprintf("Expected DNSAnswer buffer length and actual length differ: " +
+                "%v bytes expected, %v bytes actual",
+				answer_buf_cap,
+				len(answer_buf),
+			))
+	}
+
+	return answer_buf
 }
 
 func (message DNSMessage) Serialize() []byte {
 	header_buf := message.header.Serialize()
 	question_buf := message.serializeQuestions()
+	answer_buf := message.serializeAnswers()
+
+	buf := make([]byte, 0, len(header_buf) + len(question_buf) + len(answer_buf))
 	buf = append(buf, header_buf...)
 	buf = append(buf, question_buf...)
+	buf = append(buf, answer_buf...)
+
 	return buf
 }
