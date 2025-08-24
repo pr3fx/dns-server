1611f26ddf7f0c809386f557fadcf595f908909f
Jordan Collocott <jrdn.collocott@gmail.com>
2025-08-24 14:00:44 +0200

    fix(DNSAnswer): ensure IP address is in 4-byte format

--- api/dns/dns_answer_types.go
+++ api/dns/dns_answer_types.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"net"
+	"fmt"
 )
 
 // ----------------- ANSWER INTERFACES -----------------
@@ -25,8 +26,13 @@ type typeA_Answer struct {
 	ipv4_addr net.IP
 }
 
-func NewTypeA_Answer(ipv4_addr net.IP) typeA_Answer {
-	return typeA_Answer{ ipv4_addr:ipv4_addr }
+func NewTypeA_Answer(ipv4_addr net.IP) (typeA_Answer, error) {
+	// ensure ipv4_addr is 4 bytes, not 16
+	ipv4_addr_4 := ipv4_addr.To4()
+	if ipv4_addr_4 == nil {
+		return typeA_Answer{}, fmt.Errorf(`IPv4 address is not valid: %v`, ipv4_addr)
+	}
+	return typeA_Answer{ ipv4_addr:ipv4_addr_4 }, nil
 }
 
 func (data typeA_Answer) serializeToBigEndianBytes() ([]byte, uint32) {
