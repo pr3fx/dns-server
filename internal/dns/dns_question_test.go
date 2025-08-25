5316a378bb78e240fbb59d0d6cd47eaced13e81f
Jordan Collocott <jrdn.collocott@gmail.com>
2025-08-25 19:14:02 +0200

    test(DNSQuestion): test serialization

--- internal/dns/dns_question_test.go
+++ internal/dns/dns_question_test.go
@@ -24,3 +24,30 @@ func TestSetQNAME(t *testing.T) {
 		}
 	}
 }
+
+var TestVectors_SerializeQuestion = []struct{
+	question DNSQuestion
+	want []byte
+}{
+	{
+		DNSQuestion{[]byte{12,99,111,100,101,99,114,97,102,116,101,114,115,2,105,111,0}, RecordType_A, uint16(1)},
+		[]byte{12,99,111,100,101,99,114,97,102,116,101,114,115,2,105,111,0,0,1,0,1},
+	},
+	{
+		DNSQuestion{[]byte{3,119,119,119,12,99,111,100,101,99,114,97,102,116,101,114,115,2,105,111,0},RecordType_AAAA, uint16(20)},
+		[]byte{3,119,119,119,12,99,111,100,101,99,114,97,102,116,101,114,115,2,105,111,0,0,28,0,20},
+	},
+	{
+		DNSQuestion{[]byte{3,119,119,119,2,117,112,2,97,99,2,122,97,0}, RecordType_NS, uint16(255)},
+		[]byte{3,119,119,119,2,117,112,2,97,99,2,122,97,0,0,2,0,255},
+	},
+}
+
+func TestSerializeQuestion(t *testing.T) {
+	for _, testVector := range TestVectors_SerializeQuestion {
+		got_buf := testVector.question.Serialize()
+		if !reflect.DeepEqual(got_buf, testVector.want) {
+			t.Errorf(`question serialize = %v, want %v`, got_buf, testVector.want)
+		}
+	}
+}
