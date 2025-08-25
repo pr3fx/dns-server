b31537c28e00d2f0b93efe51a3bc55a3785b7aa6
Jordan Collocott <jrdn.collocott@gmail.com>
2025-08-25 19:14:39 +0200

    feat(DNSQuestion): add parsing of question bytestreams

--- internal/dns/dns_question.go
+++ internal/dns/dns_question.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"encoding/binary"
+	"fmt"
 )
 
 
@@ -47,4 +48,57 @@ func (question DNSQuestion) Serialize() []byte {
 	binary.BigEndian.PutUint16(buf[len(question.qname)+2:], question.qclass)
 
 	return buf
+}
+
+
+// Parsing functions
+func ParseQuestions(question_stream []byte, QDCOUNT uint16) ([]DNSQuestion, error) {
+	if len(question_stream) < 5 {
+		return nil, fmt.Errorf(`bytestream is %v bytes, expected 5 or more.`, len(question_stream))
+	}
+
+	stream_pos := 0
+	var question_list []DNSQuestion
+	for i := 0; i < int(QDCOUNT); i++ {
+		QNAME_len, err := getQNAMElen(question_stream[stream_pos:])
+		if err != nil {
+			return nil, err
+		}
+		QNAME_end_pos := stream_pos + int(QNAME_len)
+		// Check if stream is big enough for the QTYPE and QCLASS fields
+		if len(question_stream) <  + QNAME_end_pos + 4 {
+			return nil, fmt.Errorf(`Bytestream is truncated`)
+		}
+
+		QTYPE_parsed := RecordType(binary.BigEndian.Uint16(question_stream[QNAME_end_pos:QNAME_end_pos+2]))
+		QCLASS_parsed := binary.BigEndian.Uint16(question_stream[QNAME_end_pos+2:QNAME_end_pos+4])
+
+		// Create DNSQuestion struct from parsed values, append to slice
+		parsed_question := DNSQuestion{
+			qname:question_stream[stream_pos:QNAME_end_pos],
+			qtype:QTYPE_parsed,
+			qclass:QCLASS_parsed,
+		}
+		question_list = append(question_list, parsed_question)
+
+		// Update stream position to start at the next block
+		stream_pos = QNAME_end_pos + 4
+	}
+
+	return question_list, nil
+}
+
+func getQNAMElen(buf []byte) (uint16, error) {
+	var byte_count uint16 = 1
+	j := uint8(buf[0])
+
+	for j != 0 {
+		byte_count += uint16(j) + 1
+		if len(buf) < int(byte_count) {
+			return 0, fmt.Errorf(`QNAME malformed or bytestream truncated`)
+		}
+		j = buf[byte_count-1]
+	}
+
+	return byte_count, nil
 }
