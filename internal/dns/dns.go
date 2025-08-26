b8cf24c879a5704a23bb14c71d9d7bdf770e6eba
Jordan Collocott <jrdn.collocott@gmail.com>
2025-08-26 12:15:07 +0200

    feat(DNS): add create dns response message

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -2,6 +2,7 @@ package dns
 
 import (
 	"fmt"
+	"net"
 )
 
 type RecordType uint16
@@ -46,4 +47,46 @@ func getEncodedDomainLen(buf []byte) (uint16, error) {
 	}
 
 	return byte_count, nil
+}
+
+
+// Public DNS server functions
+func NewDNSMessageResponse(message_stream []byte) (DNSMessage, error) {
+	received_msg, err := ParseMessage(message_stream)
+	if err != nil {
+		return DNSMessage{}, err
+	}
+	// Construct response header
+	rsp_header := DNSHeader{
+		id:received_msg.header.GetID(), // mimic received ID field
+	}
+	rsp_header.SetQR(1) // set response type to response
+	rsp_header.SetOPCODE(received_msg.header.GetOPCODE())
+	rsp_header.SetAA(0)
+	rsp_header.SetTC(0)
+	rsp_header.SetRD(received_msg.header.GetRD())
+	rsp_header.SetRA(0)
+	rsp_header.SetZ(0)
+	if received_msg.header.GetOPCODE() == 0 {
+		rsp_header.SetRCODE(0) // no error
+	} else {
+		rsp_header.SetRCODE(4) // not implemented
+	}
+	// Set to 0 for now (ie. nothing in question or answer sections)
+	rsp_header.SetQDCOUNT(0)
+	rsp_header.SetANCOUNT(0)
+	rsp_header.SetNSCOUNT(0)
+	rsp_header.SetARCOUNT(0)
+
+	rsp_message := DNSMessage{header:rsp_header}
+
+	// Add questions and answers
+	rsp_message.AddQuestion(NewDNSQuestion("codecrafters.io", RecordType_A, 1))
+	dns_ans_type, err := NewTypeA_Answer(net.IPv4(8,8,8,8))
+	if err != nil {
+		return DNSMessage{}, fmt.Errorf(`Encountered error while creating dns_ans_type: %v`, err)
+	}
+	rsp_message.AddAnswer(NewDNSAnswer("codecrafters.io", 1, 60, dns_ans_type))
+
+	return rsp_message, nil
 }
